a893b722fb373afae330320d4c336444dc9f927a
Frad LEE <fradser@gmail.com>
2019-08-18 01:56:08 +0700

    feat(C09): Add x axis animation and gradient

--- 09-🍀-breathing-animation/BreathingAnimation/BreathingAnimation/ContentView.swift
+++ 09-🍀-breathing-animation/BreathingAnimation/BreathingAnimation/ContentView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ContentView: View {
     @State var scaleInOut = false
     @State var rotateInOut = false
+    @State var moveInOut = false
     var body: some View {
         ZStack {
             Rectangle()
@@ -19,45 +20,53 @@ struct ContentView: View {
             VStack {
                 Spacer()
                 ZStack {
-                    Group {
-                        ZStack {
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:-60)
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:60)
-                        }.foregroundColor(Color.green).opacity(0.5)
+                    ZStack {
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .bottom, endPoint: .top))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
                     }
-                    
-                    Group {
-                        ZStack {
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:-60)
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:60)
-                        }.foregroundColor(Color.green).opacity(0.5).rotationEffect(.degrees(60))
+                    .opacity(0.5)
+                    ZStack {
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .bottom, endPoint: .top))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
                     }
-                    
-                    Group {
-                        ZStack {
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:-60)
-                            Circle()
-                                .frame(width: 120, height: 120, alignment: .center)
-                                .offset(y:60)
-                        }.foregroundColor(Color.green).opacity(0.5).rotationEffect(.degrees(120))
+                    .opacity(0.5).rotationEffect(.degrees(60))
+                    ZStack {
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? -60 : 0)
+                        Group {
+                            Circle().fill(LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .bottom, endPoint: .top))
+                        }
+                        .frame(width: 120, height: 120, alignment: .center)
+                        .offset(y: moveInOut ? 60 : 0)
                     }
+                    .opacity(0.5).rotationEffect(.degrees(120))
                 }
                 .rotationEffect(.degrees(rotateInOut ? 90 : 0))
-                .scaleEffect(scaleInOut ? 1 : 1/8)
-                .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/4))
+                .scaleEffect(scaleInOut ? 1 : 1/4)
+                .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8))
                 .onAppear() {
                     self.rotateInOut.toggle()
                     self.scaleInOut.toggle()
+                    self.moveInOut.toggle()
                 }
                 Spacer()
                 Text("a little work with 🍀")
