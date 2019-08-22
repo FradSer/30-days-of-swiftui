82de3dd3aaab98b7dc2725e17f0acc23993c78ae
Frad LEE <fradser@gmail.com>
2019-08-22 23:12:00 +0700

    feat(C11): Add SFSymbols Previewer

---
+++ 11-📄-sfsymbols-previewer/SFSymbolsPreviewer/SFSymbolsPreviewer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SFSymbolsPreviewer
+//
+//  Created by Frad LEE on 8/22/19.
+//  Copyright © 2019 Frad LEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
