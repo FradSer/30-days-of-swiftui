ac71d22af243168c7e0ff027dd7f404e5002db8a
Frad LEE <fradser@gmail.com>
2019-08-29 00:09:18 +0700

    feat(C13): Add LiZhi Zhuangbi

---
+++ 13-🅱️-lizhi-zhuangbi/LiZhiZhuangbi/LiZhiZhuangbi/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LiZhiZhuangbi
+//
+//  Created by Frad LEE on 8/29/19.
+//  Copyright © 2019 Frad LEE. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
