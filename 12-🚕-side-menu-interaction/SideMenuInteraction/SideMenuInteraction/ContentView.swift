0eb9ef5bbfac9e171f142a6c3e5a61b42f348bb5
Frad LEE <fradser@gmail.com>
2019-08-28 23:48:22 +0700

    feat(C12): Change method of symbols
    - Add background view
    - Fix safe area animation issue

--- 12-🚕-side-menu-interaction/SideMenuInteraction/SideMenuInteraction/ContentView.swift
+++ 12-🚕-side-menu-interaction/SideMenuInteraction/SideMenuInteraction/ContentView.swift
@@ -11,40 +11,66 @@ import SwiftUI
 struct ContentView: View {
     @State private var sideMenuActived: Bool = false
     
-    var menuListIcon: String = "menu.list"
-    
-    var dynamicMenuListImage: UIImage {
-        let configuration = UIImage.SymbolConfiguration(textStyle: .body, scale: .large)
-        let image = UIImage(named: menuListIcon) ?? UIImage()
-        return image.withConfiguration(configuration)
-    }
-    
     var body: some View {
         ZStack {
-            Group {
+            BackgroundView()
+            ZStack {
                 RoundedRectangle(cornerRadius: sideMenuActived ? 32 : 0)
                     .foregroundColor(Color.green)
                     .shadow(radius: sideMenuActived ? 32 : 0)
                 VStack {
                     HStack {
                         Button(action: {self.sideMenuActived.toggle()}) {
-                            Image(uiImage: dynamicMenuListImage)
-                                .accentColor(Color.white).frame(width: 44, height: 44, alignment: .center)
-                        }.padding(.leading, 32).padding(.top, 64)
+                            Image(uiImage: menuListImage).accentColor(Color.white)
+                                .frame(width: 44, height: 44, alignment: .center)
+                                .scaleEffect(sideMenuActived ? 1.2 : 1)
+                        }.padding(.top, 64).padding(.leading, 32)
                         Spacer()
                     }
+//                    HStack {
+//                        Spacer()
+//                    }
                     Spacer()
                 }
             }
             .offset(x: sideMenuActived ? 354 : 0)
-            .scaleEffect(sideMenuActived ? 0.8 : 1)
+            .scaleEffect(sideMenuActived ? 0.7 : 1)
+            .animation(.spring(response: 0.5)).edgesIgnoringSafeArea(.all)
+            VStack {
+                Spacer()
+                Text("a little work with 🚕")
+                .foregroundColor(Color.black.opacity(0.5))
+                .font(.system(size: 17, weight: .regular))
+                .italic()
+                .padding(16)
+            }
         }
-        .animation(.spring()).edgesIgnoringSafeArea(.all)
+        
+        
+    }
+}
+
+struct BackgroundView: View {
+    var body: some View {
+        ZStack {
+            Spacer()
+                .background(Color(red: 232/255, green: 237/255, blue: 233/255)).edgesIgnoringSafeArea(.all)
+            HStack {
+//                Image(uiImage: heartImage)
+                Spacer()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundView()
     }
 }
---
+++ 12-🚕-side-menu-interaction/SideMenuInteraction/SideMenuInteraction/CustomSymbols.swift
@@ -0,0 +1,16 @@
+//
+//  CustomSymbols.swift
+//  SideMenuInteraction
+//
+//  Created by Frad LEE on 8/28/19.
+//  Copyright © 2019 Frad LEE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let menuListImage: UIImage = UIImage(named: "menu.list")!
+    .withConfiguration(UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .medium))
+
+let heartImage: UIImage = UIImage(named: "heart")!
+    .withConfiguration(UIImage.SymbolConfiguration(pointSize: 32, weight: .regular, scale: .medium))
