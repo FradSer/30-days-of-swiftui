361abf25a8721c96013ebb221073924411c66560
Frad LEE <fradser@gmail.com>
2019-09-07 23:32:01 +0700

    feat(C14): Add details
    - background color
    - footer

--- 14-🎫-lottie-showcase/LottieShowcase/LottieShowcase/View/ContentView.swift
+++ 14-🎫-lottie-showcase/LottieShowcase/LottieShowcase/View/ContentView.swift
@@ -8,13 +8,28 @@
 
 import SwiftUI
 
+let backgroundColor: Color = Color(red: 229/255, green: 240/255, blue: 239/255)
+
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            ForEach(lottieList, id: \.self) { lottie in
-                VStack(alignment: .center){
-                    LottieView(fileName: lottie).frame(height: 200)
+        ZStack {
+            Rectangle()
+                .foregroundColor(backgroundColor)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                ScrollView {
+                    ForEach(lottieList, id: \.self) { lottie in
+                        VStack(alignment: .center){
+                            LottieView(fileName: lottie).frame(height: 200)
+                        }
+                    }
                 }
+                Text("a little work with 🎫")
+                    .foregroundColor(Color.black.opacity(0.5))
+                    .font(.system(size: 17, weight: .regular))
+                    .italic()
+                    .padding(.top, 16)
+                    .padding(.bottom, 16)
             }
         }
     }
