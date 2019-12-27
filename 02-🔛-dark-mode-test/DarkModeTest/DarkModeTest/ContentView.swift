490864ebfa52db30a2f0c2cdf2d1cf0dae15be08
Frad LEE <fradser@gmail.com>
2019-12-28 02:10:01 +0700

    style(C02): Formate code

--- 02-🔛-dark-mode-test/DarkModeTest/DarkModeTest/ContentView.swift
+++ 02-🔛-dark-mode-test/DarkModeTest/DarkModeTest/ContentView.swift
@@ -9,15 +9,14 @@
 import SwiftUI
 
 extension UIViewController {
-    var isDarkModeEnabled : Bool {
-        get {
-            return traitCollection.userInterfaceStyle == .dark
-        }
+    var isDarkModeEnabled: Bool {
+        return traitCollection.userInterfaceStyle == .dark
     }
 }
+
 struct ContentView: View {
     @Environment(\.colorScheme) var colorScheme: ColorScheme
-    
+
     var body: some View {
         ZStack {
             Color(UIColor(named: "accentColor")!)
@@ -31,14 +30,13 @@ struct ContentView: View {
                             .foregroundColor(Color(UIColor(named: "labelColor")!))
                             .multilineTextAlignment(.leading)
                         Text(colorScheme == .light ? "Light   " : "Dark   ")
-                            .font(colorScheme == .light ?  Font.custom("Condiment-Regular", size: 98) : Font.custom("Superclarendon", size: 78))
+                            .font(colorScheme == .light ? Font.custom("Condiment-Regular", size: 98) : Font.custom("Superclarendon", size: 78))
                             .foregroundColor(Color(UIColor(named: "labelColor")!).opacity(0.75))
                             .multilineTextAlignment(.leading)
                             .padding(.top, 20)
                             .frame(width: 250)
                     }
                     .padding(.trailing, 80)
-                        
                 }
                 Spacer()
                 Text("a little work with 🔛")
@@ -53,15 +51,15 @@ struct ContentView: View {
 }
 
 #if DEBUG
-struct ContentView_Previews : PreviewProvider {
-   static var previews: some View {
-      Group {
-         ContentView()
-            .environment(\.colorScheme, .light)
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            Group {
+                ContentView()
+                    .environment(\.colorScheme, .light)
 
-         ContentView()
-            .environment(\.colorScheme, .dark)
-      }
-   }
-}
+                ContentView()
+                    .environment(\.colorScheme, .dark)
+            }
+        }
+    }
 #endif
